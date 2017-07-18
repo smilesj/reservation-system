add0b30e30fd04600f31cd51d6d6c594c8fec549
won <smilesj@outlook.kr>
2017-07-18 10:44:55 +0900

    지도 사이즈 변경

--- reservation-service/src/main/webapp/resources/js/map.js
+++ reservation-service/src/main/webapp/resources/js/map.js
@@ -9,7 +9,7 @@ $(function(){
 		var point = result.items[0].point.x+","+result.items[0].point.y;
 		//https://openapi.naver.com/v1/map/staticmap.bin?clientId=7HYSSZyvxLo0lyAcabtI&url=http://localhost:8080/&crs=EPSG:4326&center=127.0039365,37.5764761&level=12&w=300&h=300&baselayer=default&markers=127.0039365,37.5764761
 		
-		var mapurl = "https://openapi.naver.com/v1/map/staticmap.bin?clientId=7HYSSZyvxLo0lyAcabtI&url=http://localhost:8080/&crs=EPSG:4326&center="+point+"&level=12&w=300&h=300&baselayer=default&markers="+point;
+		var mapurl = "https://openapi.naver.com/v1/map/staticmap.bin?clientId=7HYSSZyvxLo0lyAcabtI&url=http://localhost:8080/&crs=EPSG:4326&center="+point+"&level=12&w=340&h=150&baselayer=default&markers="+point;
 		$(".detail_location .box_store_info .store_location .store_map").attr("src", mapurl);	
 	
 		$('.detail_location .box_store_info .store_location').attr("href", "http://map.naver.com/?lng="+result.items[0].point.x+"&pinTitle="+$(".store_name").html()+"&level=2&pinType=SITE&lat="+result.items[0].point.y+"&enc=utf8");
