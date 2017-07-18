34ef3908ff84b2deda641a57e1d5fee902561f98
won <smilesj@outlook.kr>
2017-07-18 14:16:21 +0900

    carousel_module 분리

--- reservation-service/src/main/webapp/resources/js/detail.js
+++ reservation-service/src/main/webapp/resources/js/detail.js
@@ -29,45 +29,6 @@ $(function(){
 		$(".detail_location").removeClass("hide");
 	});
 	
-	var carousel = (function(){
-		// private 변수, 함수 선언 	
-		var	size;
-		var	childCnt;
-		var	cnt = 0;
-		var item;
-
-		function init(){
-			this.append(this.children(".item")[0].outerHTML);
-			this.append(this.children(".item")[1].outerHTML);
-			size = this.outerWidth();
-			childCnt= this.children().length-2;
-			item = this.find(".item");
-			this.css('width', childCnt*size);
-		}
-		
-		function prevImg(){
-			if(cnt <= 0){
-				cnt = childCnt;
-				item.animate({'left': -(cnt*size)+'px'}, 0);
-			}
-			cnt--;
-			item.animate({'left': -(cnt*size)+'px'}, 'normal');
-		}
-		
-		function nextImg(){
-			cnt++;
-			item.animate({'left': -(cnt*size)+'px'}, 'normal');
-			if(cnt == childCnt){
-				cnt = 0;
-				item.animate({'left': '0px'}, 0);
-			}
-		}
-		return {
-			init : init,
-			prev : prevImg,
-			next : nextImg
-		}
-	})();
 	carousel.init.bind($(".visual_img"))();
 	$(".group_visual .prev_inn").on("click", carousel.prev.bind($(".visual_img")));
 	$(".group_visual .nxt_inn").on("click", carousel.next.bind($(".visual_img")));
